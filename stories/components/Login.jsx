0d13dafff66f5b281b0bd4500f4c2e6c7f117a1d
ksharifbd <ks@inov.io>
2017-03-08 17:28:26 +0600

    login component design completed

--- stories/components/Login.jsx
+++ stories/components/Login.jsx
@@ -5,17 +5,23 @@ import {Button} from 'react-toolbox/lib/button';
 import Logo from './Logo';
 import '../../src/stylesheet/base/base.css';
 import Styles from '../../src/modules/authentication/components/css/Login.css';
+import InputTheme from '../../src/modules/authentication/components/css/Theme/Input.css';
+import ButtonTheme from '../../src/modules/authentication/components/css/Theme/Button.css';
+
 
 export default class Login extends Component {
     render() {
         return (
             <div className={Styles.wrapper}>
                 <Logo href='#' src='./assets/images/logo/logo.svg' />
-                <form onSubmit={this.props.handleSubmit}>
-                    <Input type='email' label='Email' name='name' />
-                    <Input type='password' label='Password' name='name' />
-                    <Button type='submit' label='Log in' raised primary />
-                </form>
+                    <form onSubmit={this.props.handleSubmit}>
+                    <div className={Styles.inputs}>
+                        <Input type='email' label='Email' theme={InputTheme} />
+                        <Input type='password' label='Password' theme={InputTheme} />
+                    </div>
+                        <Button type='submit' label='Log in' raised theme={ButtonTheme}/>
+                    </form>
+
             </div>
         );
     }
