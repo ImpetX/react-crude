1b50fea65ce61b53e6a059f3ce350200c9228a74
ksharifbd <ks@inov.io>
2017-03-09 17:09:45 +0600

    react-toolbox input glitch solved

--- stories/components/Login.jsx
+++ stories/components/Login.jsx
@@ -3,6 +3,7 @@ import Input from 'react-toolbox/lib/input';
 import {Button} from 'react-toolbox/lib/button';
 
 import Logo from './Logo';
+
 import '../../src/stylesheet/base/base.css';
 import Styles from '../../src/modules/authentication/components/css/Login.css';
 import InputTheme from '../../src/modules/authentication/components/css/Theme/Input.css';
@@ -10,14 +11,31 @@ import ButtonTheme from '../../src/modules/authentication/components/css/Theme/B
 
 
 export default class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email : '',
+            password : ''
+        };
+        this.handleEmailChange = this.handleChange.bind(this, 'email');
+        this.handlePasswordChange = this.handleChange.bind(this, 'password');
+    }
+
+    handleChange(name, value) {
+        this.setState(
+            {
+                [name]: value
+            }
+        );
+    }
     render() {
         return (
             <div className={Styles.wrapper}>
                 <Logo href='#' src='./assets/images/logo/logo.svg' />
                     <form onSubmit={this.props.handleSubmit}>
                     <div className={Styles.inputs}>
-                        <Input type='email' label='Email' theme={InputTheme} />
-                        <Input type='password' label='Password' theme={InputTheme} />
+                        <Input type='email' label='Email' theme={InputTheme} value={this.state.email} onChange={this.handleEmailChange}/>
+                        <Input type='password' label='Password' theme={InputTheme} value={this.state.password} onChange={this.handlePasswordChange} />
                     </div>
                         <Button type='submit' label='Log in' raised theme={ButtonTheme}/>
                     </form>
