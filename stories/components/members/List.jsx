4a93fc270b5edb35412ab03bfb21ddd75259cc2b
ksharifbd <ks@inov.io>
2017-03-19 14:46:50 +0600

    table menu added

--- stories/components/members/List.jsx
+++ stories/components/members/List.jsx
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import {IconMenu, MenuItem, MenuDivider} from 'react-toolbox/lib/menu';
 
 import Searchbox from '../ui/Searchbox';
 import ListTable from '../ui/Table';
@@ -19,13 +18,6 @@ export default class MemberList extends Component {
                 <div style={{marginTop: '25px'}}>
                     <ListTable />
                 </div>
-                <IconMenu icon='more_vert' position='topLeft' menuRipple>
-                    <MenuItem value='download' icon='get_app' caption='Download' />
-                    <MenuItem value='help' icon='favorite' caption='Favorite' />
-                    <MenuItem value='settings' icon='open_in_browser' caption='Open in app' />
-                    <MenuDivider />
-                    <MenuItem value='signout' icon='delete' caption='Delete' disabled />
-                </IconMenu>
             </div>
         );
     }
--- stories/components/ui/Table.jsx
+++ stories/components/ui/Table.jsx
@@ -1,11 +1,31 @@
 import React, {Component} from 'react';
 import {Table, TableHead, TableRow, TableCell} from 'react-toolbox/lib/table';
+import {IconMenu, MenuItem, MenuDivider} from 'react-toolbox/lib/menu';
 
 import {data} from './Tablemodel';
 
-import Styles from '../../../src/modules/members/components/css/List.css';
+import TableTheme from '../../../src/modules/members/components/css/Theme/Table.css';
+import MenuTheme from '../../../src/modules/members/components/css/Theme/Menu.css';
 
 export default class ListTable extends Component {
+    generateTableHeader() {
+        let headItems = ['Name', 'Contact', 'Blood Group', 'Action'];
+
+        return headItems.map((item, index) => {
+            return (
+                <TableCell key={index} theme={TableTheme}>{item}</TableCell>
+            );
+        });
+    }
+    actionMenu() {
+        return (
+            <IconMenu icon='more_vert' position='topLeft' menuRipple>
+                <MenuItem caption='Edit' theme={MenuTheme}/>
+                <MenuItem caption='Delete' theme={MenuTheme}/>
+            </IconMenu>
+        );
+    }
+
     getTableRow() {
         return data.map((item, index) => {
             return (
@@ -13,18 +33,20 @@ export default class ListTable extends Component {
                     <TableCell>{item.name}</TableCell>
                     <TableCell>{item.contact}</TableCell>
                     <TableCell>{item.bloodGroup}</TableCell>
+                    <TableCell>
+                        {this.actionMenu()}
+                    </TableCell>
                 </TableRow>
             );
         });
     }
 
     render() {
+        console.log('TableTheme', TableTheme);
         return (
-            <Table selectable={false}>
+            <Table selectable={false} theme={TableTheme}>
                 <TableHead>
-                    <TableCell>Name</TableCell>
-                    <TableCell>Contact</TableCell>
-                    <TableCell>Blood Group</TableCell>
+                    {this.generateTableHeader()}
                 </TableHead>
                 {this.getTableRow()}
             </Table>
