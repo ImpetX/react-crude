206b7fc430f4f3e8bf1a197a07e19ecce5bc245b
ksharifbd <ks@inov.io>
2017-03-20 11:30:51 +0600

    list pagination component added

--- stories/components/members/List.jsx
+++ stories/components/members/List.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 
 import Searchbox from '../ui/Searchbox';
 import ListTable from '../ui/Table';
+import Pagination from '../ui/pagination/Pagination';
 
 import '../../../node_modules/normalize.css/normalize.css';
 import '../../../src/stylesheet/base/base.css';
@@ -15,8 +16,14 @@ export default class MemberList extends Component {
                     <h1>Members <span>/</span> <span>List</span></h1>
                 </div>
                 <Searchbox />
-                <div style={{marginTop: '25px'}}>
+                <div style={{marginTop: '25px', marginBottom: '25px'}}>
                     <ListTable />
+                </div>
+                <div>
+                    <div>
+                        <p>There are total 1000 members</p>
+                    </div>
+                    <Pagination />
                 </div>
             </div>
         );
