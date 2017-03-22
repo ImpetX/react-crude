78c91ee576ae5c44ec6dfee5279a0562af6a1bc7
ksharifbd <ks@inov.io>
2017-03-22 16:29:10 +0600

    logo presentational component changed to stateless functional components

--- stories/components/Logo.jsx
+++ stories/components/Logo.jsx
@@ -2,18 +2,17 @@ import React, {Component} from 'react';
 
 import Styles from '../../src/modules/authentication/components/css/Logo.css';
 
-export default class Logo extends Component {
-    render() {
-        return (
-            <a href={this.props.href} className={Styles.wrapper}>
-                <img src={this.props.src} alt='logo' className={Styles.logo} />
-            </a>
-        );
-    }
-}
+const Logo = ({href, src}) => {
+    return (
+        <a href={href} className={Styles.wrapper}>
+            <img src={src} alt='logo' className={Styles.logo} />
+        </a>
+    );
+};
 
 Logo.propTypes = {
     href: React.PropTypes.string,
-    src: React.PropTypes.string,
-    className: React.PropTypes.string
+    src: React.PropTypes.string
 }
+
+export default Logo;
