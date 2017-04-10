35f5127c272eb792fba9313c868b24454f9ad8df
ksharifbd <ks@inov.io>
2017-04-10 17:56:28 +0600

    react-router-dom added & basic routing implemented

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,20 +1,15 @@
-// import React, {Component} from 'react';
-// import {Router, Route, browserHistory, IndexRoute} from 'react-router';
-//
-// import Home from './modules/home/components/Home';
-// import Hello from './modules/hello/components/Hello';
-// import Kamal from './modules/persons/components/Kamal';
-// import Jamal from './modules/persons/components/Jamal';
-//
-// export default class Routes extends Component {
-//     render() {
-//         return (
-//             <Router history={browserHistory}>
-//                 <Route path='/' component={Home} >
-//                     <Route path='/kamal' component={Kamal} />
-//                     <Route path='/jamal' component={Jamal} />
-//                 </Route>
-//             </Router>
-//         );
-//     }
-// }
+import React from 'react';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+
+import Login from './modules/authentication/components/Login';
+
+const Routes = () => (
+    <BrowserRouter>
+        <div>
+            <Link to='/login'>Login</Link>
+            <Route path='/login' component={Login} />
+        </div>
+    </BrowserRouter>
+);
+
+export default Routes;
