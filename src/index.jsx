637e0bf7358eb523cc3dea145ca539b55a229e4d
ksharifbd <ks@inov.io>
2017-04-14 11:07:42 +0600

    react-redux binding done

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import configureStore from './store';
 
 import Routes from './routes';
 
+const store = configureStore();
 const app = document.getElementById('app');
 
-ReactDOM.render(<Routes />, app);
+ReactDOM.render(
+    <Provider store={store}>
+        <Routes />
+    </Provider>,
+    app
+);
