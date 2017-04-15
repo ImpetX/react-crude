ac10c6959b64079b27e7cf9a6a525567796e0800
ksharifbd <ks@inov.io>
2017-04-15 16:42:34 +0600

    firebase javascript sdk & config added

---
+++ src/firebase/config.jsx
@@ -0,0 +1,10 @@
+const config = {
+    apiKey: "AIzaSyAjIy62ACyUq20km8GDISjKMdLdmOe3UrM",
+    authDomain: "react-crud-e3c14.firebaseapp.com",
+    databaseURL: "https://react-crud-e3c14.firebaseio.com",
+    projectId: "react-crud-e3c14",
+    storageBucket: "react-crud-e3c14.appspot.com",
+    messagingSenderId: "868643763364"
+};
+
+export default config;
