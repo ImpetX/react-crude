eeb498669a7ff1da1a0cbc861b467c692bff9cbb
ksharifbd <ks@inov.io>
2017-04-19 15:43:15 +0600

    firebase user created

--- src/modules/authentication/actions/index.jsx
+++ src/modules/authentication/actions/index.jsx
@@ -1,18 +1,14 @@
 import ActionTypes from './types';
 
-function isLoginAttempt(email, password) {
+function isLoginAttempt() {
     return {
         type: ActionTypes.ATTEMPTING_LOGIN,
-        isLoggedIn: false,
-        email,
-        password
     };
 }
 
 function isLoginSuccess(email, password) {
     return {
         type: ActionTypes.LOGGED_IN,
-        isLoggedIn: true,
         email,
         password
     };
@@ -21,7 +17,6 @@ function isLoginSuccess(email, password) {
 function isLoginFailed(email, password) {
     return {
         type: ActionTypes.LOGGED_IN_ERROR,
-        isLoggedIn: false,
         email,
         password
     };
@@ -29,7 +24,12 @@ function isLoginFailed(email, password) {
 
 function isLogOut() {
     return {
-        type: ActionTypes.LOGOUT,
-        isLoggedIn: false
+        type: ActionTypes.LOGOUT
     };
+}
+
+function attemptToLogin(email, password) {
+    return function(dispatch) {
+        dispatch(isLoginAttempt());
+    }
 }
