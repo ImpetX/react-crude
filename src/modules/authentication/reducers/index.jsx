f5a689e84647f0d7396e4ce88beb9b74b6f7568f
ksharifbd <ks@inov.io>
2017-04-22 19:01:13 +0600

    auth pending state added for showing UI loader

--- src/modules/authentication/reducers/index.jsx
+++ src/modules/authentication/reducers/index.jsx
@@ -3,35 +3,45 @@ import ActionTypes from '../constants';
 const defaultState = {};
 
 const isLoggedInTrue = {
-    isLoggedIn: true,
+    isLoggedIn: true
+};
+
+const isLoggedInFalse = {
+    isLoggedIn: false
+};
+
+const isAuthInProcess = {
+    isLoggedIn: 'pending'
+}
+
+const authSuccessState = {
     uid: action.payload.uid,
     email: action.payload.email,
     displayName: 'Admin'
 };
 
-const isLoggedInFalse = {
-    isLoggedIn: false,
+const authFailedState = {
     uid: null,
     email: null,
     displayName: null
-};
+}
 
 const AuthReducer = (state = defaultState, action) => {
     switch(action.type) {
         case ActionTypes.ATTEMPTING_LOGIN:
-            return Object.assign({}, state, isLoggedInFalse);
+            return Object.assign({}, state, isAuthInProcess);
 
         case ActionTypes.LOGGED_IN:
-            return Object.assign({}, state, isLoggedInTrue);
+            return Object.assign({}, state, isLoggedInTrue, authSuccessState);
 
         case ActionTypes.LOGGED_IN_ERROR:
-            return Object.assign({}, state, isLoggedInFalse);
+            return Object.assign({}, state, isLoggedInFalse, authFailedState);
 
         case ActionTypes.LOGOUT_SUCCESS:
-            return Object.assign({}, state, isLoggedInFalse);
+            return Object.assign({}, state, isLoggedInFalse, authFailedState);
 
         case ActionTypes.LOGOUT_ERROR:
-            return Object.assign({}, state, isLoggedInFalse);
+            return Object.assign({}, state, isLoggedInFalse, authFailedState);
 
         default:
             return state;
