d04580daf408a72203f0ee939099c608eb3cea75
ksharifbd <ks@inov.io>
2017-04-23 21:55:34 +0600

    public & private route separated

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,15 +1,30 @@
 import React, {Component} from 'react';
-import {Router, Route, browserHistory, IndexRoute} from 'react-router';
+import {Router, Route, browserHistory} from 'react-router';
 
 import 'styles/main';
 import Login from 'modules/authentication/components/Login';
+import Layout from 'modules/core/components/layout/Layout';
+import NotFound from 'modules/core/components/layout/NotFound';
 
-export default class Routes extends Component {
-    render() {
-        return (
-            <Router history={browserHistory}>
-                <Route path='/' component={Login} />
-            </Router>
-        );
-    }
-}
+const Public = () => {
+    return (
+        <Router history={browserHistory}>
+            <Route path='/' component={Login} />
+            <Route path="*" component={NotFound}/>
+        </Router>
+    );
+};
+
+const Private = () => {
+    return (
+        <Router history={browserHistory}>
+            <Route path="/home" component={Layout} />
+            <Route path="*" component={NotFound}/>
+        </Router>
+    );
+};
+
+export {
+	Public,
+	Private
+};
