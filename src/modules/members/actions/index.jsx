ab8aebd65ceec0f9a52465841e21531f3a5d78ff
ksharifbd <ks@inov.io>
2017-05-30 22:45:24 +0600

    firebase storage understood

--- src/modules/members/actions/index.jsx
+++ src/modules/members/actions/index.jsx
@@ -7,10 +7,22 @@ function isMemberAddAttempt() {
     }
 }
 
-function isMemberAddSuccess(memberData) {
+function isImageUploadSuccess(snapshot) {
     return {
-        type: ActionTypes.MEMBER_ADD_SUCCESS,
-        memberData
+        type: ActionTypes.IMAGE_UPLOAD_SUCCESS,
+        snapshot
+    }
+}
+
+function isImageUploadError() {
+    return {
+        type: ActionTypes.IMAGE_UPLOAD_ERROR
+    }
+}
+
+function isMemberAddSuccess() {
+    return {
+        type: ActionTypes.MEMBER_ADD_SUCCESS
     }
 }
 
@@ -20,32 +32,39 @@ function isMemberAddError() {
     }
 }
 
-function memberAddProcess(memberData) {
+function memberAddProcess(memberData, imageFile) {
     return dispatch => {
         dispatch(isMemberAddAttempt());
 
-        let membersRef = ref.child('members');
-        let newMemberRef = membersRef.push();
+        let imageRef = storageRef.child('images');
+        let memberImageRef = imageRef.child(`${imageFile}`);
+        let uploadImage = memberImageRef.put(imageFile);
+        uploadImage.then(snapshot => {
+            dispatch(isImageUploadSuccess(snapshot));
 
-        newMemberRef.set({
-            bengaliName: memberData.bengaliName,
-            englishName: memberData.englishName,
-            fatherName: memberData.fatherName,
-            motherName: memberData.motherName,
-            presentAddress: memberData.presentAddress,
-            permanentAddress: memberData.permanentAddress,
-            occupation: memberData.occupation,
-            mobileNumber: memberData.mobileNumber,
-            birthDate: memberData.birthDate,
-            maritalStatus: memberData.maritalStatus,
-            nationality: memberData.nationality,
-            religion: memberData.religion,
-            bloodGroup: memberData.bloodGroup,
-            referrerName: memberData.referrerName,
-            referrerContact: memberData.referrerContact,
-            certificateType: memberData.certificateType,
-            certificateNumber: memberData.certificateNumber,
-            membershipDate: memberData.membershipDate
+            let membersRef = ref.child('members');
+            let newMemberRef = membersRef.push();
+
+            newMemberRef.set({
+                bengaliName: memberData.bengaliName,
+                englishName: memberData.englishName,
+                fatherName: memberData.fatherName,
+                motherName: memberData.motherName,
+                presentAddress: memberData.presentAddress,
+                permanentAddress: memberData.permanentAddress,
+                occupation: memberData.occupation,
+                mobileNumber: memberData.mobileNumber,
+                birthDate: memberData.birthDate,
+                maritalStatus: memberData.maritalStatus,
+                nationality: memberData.nationality,
+                religion: memberData.religion,
+                bloodGroup: memberData.bloodGroup,
+                referrerName: memberData.referrerName,
+                referrerContact: memberData.referrerContact,
+                certificateType: memberData.certificateType,
+                certificateNumber: memberData.certificateNumber,
+                membershipDate: memberData.membershipDate
+            })
         })
 
     }
